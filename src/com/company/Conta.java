d2077e3c8dbcad1591e3a02d9960d1c256ca1fa9
eduardoLemes <eduardo_lsf@outlook.com>
2022-01-17 23:00:27 -0300

    transferencia realizada

--- src/com/company/Conta.java
+++ src/com/company/Conta.java
@@ -12,15 +12,25 @@ public class Conta{
 
     }
 
-    public boolean sacar (double valor){
-        if(this.saldo >= valor){
+    public boolean sacar (double valor) {
+        if (this.saldo >= valor) {
             this.saldo -= valor;
             return true;
-        } else{
-            return false;
+        }
+        return false;
+    }
+
+    public boolean transfere(double valor, Conta destino){
+         if(this.saldo >= valor){
+             this.saldo -= valor;
+             destino.deposita(valor);
+             return true;
+         }
+          return false;
+
         }
 
     }
-}
+
 
 
--- src/com/company/TesteMetodos.java
+++ src/com/company/TesteMetodos.java
@@ -7,12 +7,25 @@ public class TesteMetodos {
         contaDoEdu.saldo=100;
 
         contaDoEdu.deposita(50);
-        System.out.print(contaDoEdu.saldo);
+        System.out.print("\n"+ contaDoEdu.saldo);
 
 
         boolean sacou = contaDoEdu.sacar(20);
-        System.out.print(contaDoEdu.saldo);
-        System.out.print(sacou);
+        System.out.print("\n"+ contaDoEdu.saldo);
+        System.out.print("\n"+ sacou);
+
+        Conta contadaPalmoa = new Conta();
+        contadaPalmoa.deposita(1000);
+
+        boolean sucessoTransferencia = contadaPalmoa.transfere(300, contaDoEdu);
+
+        if (sucessoTransferencia){
+            System.out.print("\n Transferencia com sucesso");
+        }else{
+            System.out.print("\n Faltou dinheiro");
+        }
+        System.out.print("\n"+ contaDoEdu.saldo);
+        System.out.print("\n"+ contadaPalmoa.saldo);
     }
 }
 
